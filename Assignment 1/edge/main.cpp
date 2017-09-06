a07bd14de584ae8edbba685749d985a4dda3f0d3
Lars Jaeqx <lars.jaeqx@gmail.com>
2017-09-06 07:28:39 -0500

    Part 1 assignment 1

--- Assignment 1/edge/main.cpp
+++ Assignment 1/edge/main.cpp
@@ -2,78 +2,83 @@
 
 using namespace cv;
 
-//main functions
-void processImage();
+void processImage4();
+void processImage8();
+void processImageUsingOpenCVFunctions();
+void processImageGradient();
 void displayGraphics();
 
-//images
-Mat image;
-Mat processedImage;
+Mat image, processedImage4, processedImage8, processedImageCV, processedGradient, kernel;
+int kernel_size = 3;
+int scale = 4;
+int delta = 0;
+int ddepth = -1;
 
 int main(int argc, char *argv[])
 {
-//create a window
-namedWindow("Image");
-namedWindow("Processed Image");
+  namedWindow("Image");
+  namedWindow("Processed Image 4");
+  namedWindow("Processed Image 8");
+  namedWindow("Processed openCV");
+  namedWindow("Processed Gradient");
 
 //load the image
-if(argc > 1)
-	image = imread(argv[1]);	
-else
-	image = imread("lena.jpg");	
+  if(argc > 1)
+   image = imread(argv[1]);	
+ else
+   image = imread("edge detection.jpg");	
 
-if(image.empty())
-	exit(1);
-
-
-processImage();
-displayGraphics();
-
-waitKey(0);
+ if(image.empty())
+   exit(1);
 
 
-//no need to release memory
-return 0;
+ processImage4();
+ processImage8();
+ processImageUsingOpenCVFunctions();
+ processImageGradient();
+ displayGraphics();
+
+ waitKey(0);
+ return 0;
 }
-
-
-
 
 void displayGraphics()
 {
-//display both images
-imshow("Image", image);
-imshow("Processed Image", processedImage);
+  imshow("Image", image);
+  imshow("Processed Image 4", processedImage4);
+  imshow("Processed Image 8", processedImage8);
+  imshow("Processed openCV", processedImageCV);
+  imshow("Processed Gradient", processedGradient);
 }
 
-
-
-
-void processImage()
+void processImageUsingOpenCVFunctions()
 {
-int x,y;
-Vec3b pixel;
-unsigned char R, G, B;
-processedImage = image.clone();
-
-for(y = 0; y < processedImage.rows; y++)
- for(x = 0; x < processedImage.cols; x++)
- {
-  //Get the pixel at (x,y)
-  pixel = processedImage.at<Vec3b>(y, x);
-  //get the separate colors
-  B = pixel[0];
-  G = pixel[1];
-  R = pixel[2];
-  //assign the complement to each color
-  pixel[0] = 255 - B;
-  pixel[1] = 255 - G;
-  pixel[2] = 255 - R;
-  //write the pixel back to the image
-  processedImage.at<Vec3b>(y, x) = pixel;
- }
+  GaussianBlur( image, processedImageCV, Size(3,3), 0, 0, BORDER_DEFAULT );
+  cvtColor( processedImageCV, processedImageCV, CV_BGR2GRAY );
+  Laplacian( processedImageCV, processedImageCV, ddepth, kernel_size, scale, delta, BORDER_DEFAULT );
+  convertScaleAbs( processedImageCV, processedImageCV );
 }
 
+void processImage4()
+{
+    kernel_size = 3;
+    kernel = (Mat_<double>(3,3) << 0, -1, 0, -1, 4, -1, 0, -1, 0);
+    cvtColor( image, processedImage4, CV_BGR2GRAY );
+    filter2D(processedImage4, processedImage4, ddepth , kernel, Point( -1, -1 ), delta, BORDER_DEFAULT );
+}
 
+void processImage8()
+{
+    kernel_size = 3;
+    kernel = (Mat_<double>(3,3) << 0, -1, 0, -1, 8, -1, 0, -1, 0);
+    cvtColor( image, processedImage8, CV_BGR2GRAY );
+    filter2D(processedImage8, processedImage8, ddepth , kernel, Point( -1, -1 ), delta, BORDER_DEFAULT );
+}
 
-
+void processImageGradient()
+{
+    kernel_size = 3;
+    kernel = (Mat_<double>(3,3) << -1, 0, 1, -1, 0, 1, -1, 0, 1);
+    cvtColor( image, processedGradient, CV_BGR2GRAY );
+    filter2D(processedGradient, processedGradient, ddepth , kernel, Point( -1, -1 ), delta, BORDER_DEFAULT );
+}
