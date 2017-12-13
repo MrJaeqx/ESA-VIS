b540f9436b3106ac13fb0472764ca482a9677803
mt <minhtrietdiep@gmail.com>
2017-12-13 10:47:59 +0100

    Iets met video pt2

--- Assignment9/Distance/main.cpp
+++ Assignment9/Distance/main.cpp
@@ -4,6 +4,7 @@ using namespace cv;
 //default capture width and height
 const int FRAME_WIDTH = 640;
 const int FRAME_HEIGHT = 480;
+const std::string windowName = "Distance";
 
 struct CameraParams {
 	CameraParams(Mat matrix, Mat coeff, double focal, double res) : 
@@ -17,7 +18,7 @@ struct CameraParams {
 	double resolution;
 };
 
-void checkerboardDistance(CameraParams params, Mat image, bool verbose = false) {
+double checkerboardDistance(CameraParams params, Mat &image, bool verbose = false) {
 	Mat undistortedImage;
 
 	undistort(image, undistortedImage, params.camera_matrix, params.distortion_coefficients);
@@ -37,12 +38,20 @@ void checkerboardDistance(CameraParams params, Mat image, bool verbose = false) 
 		foundBoardCorners.push_back(Point3f(p.x, p.y, 0.0f));
 	}
 
+	std::vector<std::vector<Point>> ptVecVec = { ptvec };
+	Scalar color = Scalar( 0, 0, 255 );
+	if (found) drawContours( image, ptVecVec, 0, color, 1, 8, std::vector<Vec4i>(), 0, Point() );
+
 	cv::RotatedRect box = cv::minAreaRect(cv::Mat(ptvec));
 	double size = box.size.width / params.resolution;
 	if (verbose) std::cout << "size: " << size << "\n";
 
 	double dist = 200.0 * params.focal / size;
 	std::cout << "dist: " << dist << "\n";
+
+	if (found) putText(image, "Dist: " + std::to_string(dist), ptvec[0], FONT_HERSHEY_SIMPLEX, 0.5, color, 1, CV_AA, false);
+
+	return dist;
 }
 
 void checkerboardVideo(CameraParams params) {
@@ -63,7 +72,12 @@ void checkerboardVideo(CameraParams params) {
 			capture.set(CV_CAP_PROP_POS_FRAMES, 0);
 			continue;
 		}
-		checkerboardDistance(params, image);
+		double dist = checkerboardDistance(params, image);
+
+		imshow(windowName, image);
+		//delay 30ms so that screen can refresh.
+		//image will not appear without this waitKey() command
+		waitKey(30);
 	}
 }
 
